08e0b6d044b87892e84833196914d548ac61c7b6
Bektur <itsbektur@gmail.com>
2024-06-29 01:34:58 +0600

    feat: add a design to the Navbar component

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navbar.css";
+import { assets } from "../../assets/assets.js";
 
 const Navbar = () => {
-  return <div className="navbar">Navbar</div>;
+  const [menu, setMenu] = useState("home");
+  return (
+    <div className="navbar">
+      <img src={assets.logo} alt="logo pic" className="logo" />
+      <ul className="navbar-menu">
+        <li
+          onClick={() => setMenu("home")}
+          className={menu === "home" ? "active" : ""}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => setMenu("menu")}
+          className={menu === "menu" ? "active" : ""}
+        >
+          Menu
+        </li>
+        <li
+          onClick={() => setMenu("mobile-app")}
+          className={menu === "mobile-app" ? "active" : ""}
+        >
+          Mobile App
+        </li>
+        <li
+          onClick={() => setMenu("contact-us")}
+          className={menu === "contact-us" ? "active" : ""}
+        >
+          Contact us
+        </li>
+      </ul>
+      <div className="navbar-right">
+        <img src={assets.search_icon} alt="" />
+        <div className="navbar-search-icon">
+          <img src={assets.basket_icon} alt="" />
+          <div className="dot"></div>
+        </div>
+        <button>Sign in</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
